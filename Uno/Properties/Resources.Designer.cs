1629e54f0203cb138bc827073ed5dc88d994372c
Niek <berkhofniek0@gmail.com>
2024-03-06 11:28:24 +0100

    First design changes

--- Uno/Properties/Resources.Designer.cs
+++ Uno/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace Uno.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap UNO_Logo_svg {
+            get {
+                object obj = ResourceManager.GetObject("UNO_Logo.svg", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap UnoBack {
             get {
                 object obj = ResourceManager.GetObject("UnoBack", resourceCulture);
